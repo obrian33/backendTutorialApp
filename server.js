8f4e8902fd95395db9a3d1da57fcf86dd4c9c16b
O'Brian Santos <obrians@uci.edu>
2018-08-04 17:11:55 -0700

    Added get and post routes

---
+++ server.js
@@ -0,0 +1,56 @@
+import { express } from 'express';
+import { cors } from 'cors';
+import { bodyparser } from 'body-parser';
+import { mongoose } from 'mongoose';
+import { Issue } from 'models/Issue';
+
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyparser.json());
+
+mongoose.connect('mongodb://[server]/issues');
+
+const connection = mongoose.connection;
+
+connection.open('open', () => {
+    console.log('Mongo working');
+});
+
+app.use('/', router);
+
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issues);
+        }
+    });
+});
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issue);
+        }
+    });
+});
+
+router.route('/issues/add').post((req, res) => {
+    let issue = new Issue(req.body);
+    issue.save()
+        .then(issue => {
+            res.status(200).json({'issue': 'Added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new record');
+        });
+});
+
+app.listen(4000, () => {
+    console.log('Express server on port 4000');
+});
